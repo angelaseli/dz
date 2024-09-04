75ee74933533e0b425db0d8fb15510f022a5ff15
angelaseli <angil69@ya.ru>
2024-09-04 15:14:52 +0700

    Add files via upload
    Практическая работа по уроку "Динамическая типизация"

---
+++ dz.py
@@ -0,0 +1,30 @@
+#1st program
+print ((9 ** 0.5) * 5 )
+#2nd program
+print (9.99 > 9.98 and 100 != 1000.1)
+#3rd program
+print (( 2 * ( 2 + 2 )) == ( (2 * 2 ) + 2 ))
+#4th program
+print ((123.456 / 1.0 ) * 10)
+print (int (1234.56 % 10))
+
+A = 'Топинамбур'
+print (A[0])
+print (A[-1])
+print (A[5:10])
+print (A[::-1])
+print (A [1:10:2])
+
+#module_1_3
+name = "Angela"
+print (name, type(name))
+age = 32
+print (age, type(age))
+new_age = 5
+print (age + new_age)
+name = 'is_student'
+print (name, name == name)
+
+
+
+
