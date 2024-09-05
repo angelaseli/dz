4f1e8089fe882a231b9004b0d08ccc5af9d98b97
angelaseli <angil69@ya.ru>
2024-09-05 09:51:03 +0700

    Add files via upload
    module_1_4

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+#module_1_4
+name = input ("Как тебя зовут? " )
+print ('Привет,' , name. upper())
+print ("Привет," , name. lower())
+print ((input('Как тебя зовут? ')) .replace(' ', ''))
+print(name [0])
+print (name[-1])
