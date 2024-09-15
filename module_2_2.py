cb19979505690b0fca51e0ef8cd0a482da8eaea6
angelaseli <angil69@ya.ru>
2024-09-15 22:15:32 +0700

    15.09

---
+++ module_2_2.py
@@ -0,0 +1,10 @@
+number = int(input('first: '))
+numbert = int(input('second: '))
+numberf = int(input('third: '))
+if number == numbert and number == numberf:
+    print('3')
+elif number == numbert or number == numberf:
+    print('2')
+elif number != numbert and number != numberf:
+    print('0')
+    
