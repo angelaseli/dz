7982c40c7a77fea9c89e4d661bbe4c7109feab9f
angelaseli <angil69@ya.ru>
2024-09-20 00:11:48 +0700

    Add files via upload
    Задание "Слишком древний шифр"

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,25 +1,12 @@
 numbers = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
-def password (number):
-    passwo = []
-    for i in range(1, number):
-        for j in range(i + 1, number + 1):
-            if number % (i + j) == 0:
-                passwo = i + j
-                return passwo
-
-
+def password (numbers):
+    passwo = ''
+    for i in range(1, numbers):
+        for j in range(i + 1, numbers):
+            if numbers % (i + j) == 0:
+                passwo += str(i) + str(j)
+    return passwo
 
 print(password(int(input('Введите пароль от 3 до 20:'))))
-
-
-
-
-
-
-
-
-
-
-
 
