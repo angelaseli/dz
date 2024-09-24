e25604bc63ece90d66a1172a47bab701bd964b1b
angelaseli <angil69@ya.ru>
2024-09-24 19:16:35 +0700

    Домашняя работа по уроку "Способы вызова функции"

---
+++ module_3_2.py
@@ -0,0 +1,27 @@
+
+def send_email(message, recipient, sender="university.help@gmail.com"):
+    domains = ['@','.com', '.ru', '.net']
+    recipient_domain = recipient[recipient.rfind('.'):]
+    sender_domain = sender[sender.rfind('.'):]
+    if recipient_domain not in domains or sender_domain not in domains:
+        print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
+
+    elif recipient == sender:
+        print("Нельзя отправить письмо самому себе!")
+    elif sender == "university.help@gmail.com":
+        print("Письмо успешно отправлено с адреса", sender, "на адрес", recipient)
+    else:
+        print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса", sender, "на адрес", recipient)
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
+
+# elif ".com" not in recipient or sender:
+# print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
+# elif ".ru" not in recipient or sender:
+# print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
+# #elif sender[index_2:] in domens:
+#         #print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
